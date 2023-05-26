baa2c783cb96b4d471ef48ba0f5fbf1f4c69bb28
Emrehan <emrehan.kisacik@hotmail.com>
2023-05-27 00:45:36 +0300

    add sign up sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+    before_action :authorize
+
+    private
+
+    def authorize
+        @current_user = User.find_by(id: session[:user_id])
+
+        render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+    end
+
+    def render_unprocessable_entity_response
+        render json: { errors: exception.record.errors.full_messages }
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  post "/signup",to: "users#create"
+  get "/me",to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to:"session#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
