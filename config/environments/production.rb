f89fa9828d8fb4c9f1eda071ea3c055913a74128
Emrehan <emrehan.kisacik@hotmail.com>
2023-05-29 22:08:51 +0300

    some fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.hosts << "2368-85-98-225-95.ngrok-free.app/login"
 end
