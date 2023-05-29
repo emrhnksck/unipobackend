5e7343f8abdf5d5b1c6dcad46e32ef83f9f0e6b0
Emrehan <emrehan.kisacik@hotmail.com>
2023-05-29 21:27:17 +0300

    deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # post "/signup",to: "users#create"
   # get "/me",to: "users#show"
-  # post "/login", to: "sessions#create"
-  # delete "/logout", to:"session#destroy"
+  post "/login", to: "sessions#create"
+  delete "/logout", to:"session#destroy"
 
   resources :users do
     resources :projects, only: [:index, :create]
